fb2d01e396525d5519711f5e038774d9c0b261e6
Muhammad Faiaz <68774895+Faiax@users.noreply.github.com>
2022-11-12 11:39:43 +0600

    Add files via upload

---
+++ 50A-Domino piling.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+    int M{0};
+    int N{0};// decleration 
+    cin >> M >> N; // imputting
+    
+    int ans = (M*N)/2; // condition
+    cout << ans;// output
+
+
+}
