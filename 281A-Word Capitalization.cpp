a847a4402a9dee11f3a134545af71fd641c59188
Muhammad Faiaz <68774895+Faiax@users.noreply.github.com>
2022-11-14 15:05:47 +0600

    Add files via upload

---
+++ 281A-Word Capitalization.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+#include <string>
+using namespace std;
+    
+
+int main(){
+    
+    string s;
+    cin >> s;
+    
+    
+    s[0]= toupper(s[0]); // this code capatilzaies every character
+    cout << s;
+    
+    return 0;}
