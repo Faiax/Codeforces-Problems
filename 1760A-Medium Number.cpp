2d93c26d8065683f1d25e292f71a3aee889df39c
Muhammad Faiaz <68774895+Faiax@users.noreply.github.com>
2022-12-06 22:18:32 +0600

    Add files via upload

---
+++ 1760A-Medium Number.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+
+    int n{};
+    cin >> n;
+
+    while(n--){
+
+        int arr[3];
+
+        cin >> arr[0] >> arr[1]>> arr[2];
+
+        sort(arr, arr+3);
+
+        cout <<  arr[1]<< endl;
+
+    }
+
+    return 0;
+}
